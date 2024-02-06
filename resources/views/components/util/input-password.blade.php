d8564cf558c2056cf812c1247e1e5393c47aff51
Masud Al Imran <masudalimran92@gmail.com>
2024-02-06 11:00:09 +0600

    refact(password-input): updated visual for show password mode

--- resources/views/components/util/input-password.blade.php
+++ resources/views/components/util/input-password.blade.php
@@ -4,8 +4,10 @@
     <div class="relative">
         <input :type="type"
             {{ $attributes->merge(['class' => 'w-full my-2 px-4 py-2 rounded-full outline-none border-white focus:border-primary border-2']) }}
-            name="{{ $name ?? 'password' }}" placeholder="{{ $placeholder ?? '******' }}" value="{{ old($name ?? '') }}" />
-        <p class="absolute right-12 bottom-0 h-full flex items-center" x-show="type === 'text'" @click="type = 'password'">
+            :class="{ 'border-orange-500 focus:border-orange-500': type === 'text' }" name="{{ $name ?? 'password' }}"
+            placeholder="{{ $placeholder ?? '******' }}" value="{{ old($name ?? '') }}" />
+        <p class="absolute right-12 bottom-0 h-full flex items-center" x-show="type === 'text'"
+            @click="type = 'password'">
             <x-feathericon-eye-off class="text-gray-500 hover:text-black cursor-pointer transition " />
         </p>
         <p class="absolute right-12 bottom-0 h-full flex items-center" x-show="type === 'password'"

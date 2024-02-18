a72e0ee8efe1aa956ba724892665a2eca70ff339
Masud Al Imran <masudalimran92@gmail.com>
2024-02-18 11:34:00 +0600

    refact: changed menubar style

--- resources/views/components/admin-panel/header/menu-bar.blade.php
+++ resources/views/components/admin-panel/header/menu-bar.blade.php
@@ -6,7 +6,7 @@
 <ul class="flex items-center gap-6 text-xl font-thin">
     @foreach ($menus as $menuKey => $menuName)
         <a href="/admin/dashboard/{{ $menuKey }}"
-            class="{{ $path === $menuKey ? 'text-black underline' : 'hover:underline cursor-pointer' }}">
+            class="{{ $path === $menuKey ? 'px-4 py-1 bg-white text-primary font-extrabold rounded-full' : 'hover:underline cursor-pointer' }}">
             <li>
                 {{ $menuName }}
             </li>

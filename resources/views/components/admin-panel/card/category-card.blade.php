3df633eeb4847344a0769ada6a3bcffe5a461e2f
Masud Al Imran <masudalimran92@gmail.com>
2024-02-24 11:56:38 +0600

    refactor: fixed date time not showing properly

--- resources/views/components/admin-panel/card/category-card.blade.php
+++ resources/views/components/admin-panel/card/category-card.blade.php
@@ -3,9 +3,9 @@
     class="basis-1/3 py-4 flex gap-2 justify-start items-start hover:shadow-lg rounded-lg p-2 transition duration-300">
     <div class="flex flex-col justify-start items-start gap-1">
         <p class="uppercase font-semibold text-sm">{{ $category->name }}</p>
-        <p class="lowsercase font-thin text-xs flex-center gap-2">
+        <p class="lowsercase font-thin text-sm flex-center gap-2">
             Created:
-            {{ \Carbon\Carbon::parse($category->published_at)->diffForHumans() }}
+            {{ \Carbon\Carbon::parse($category->created_at)->toDayDateTimeString() }}
         </p>
     </div>
 </article>
--- resources/views/components/post/avatar-card.blade.php
+++ resources/views/components/post/avatar-card.blade.php
@@ -1,4 +1,7 @@
-@props(['post'])
+@php
+    use Carbon\Carbon;
+@endphp
+@props(['post', 'isBackend'])
 <article
     class="basis-1/3 py-4 flex gap-2 justify-start items-start hover:shadow-lg rounded-lg p-2 transition duration-300">
     <a href="/posts/{{ $post->id }}">
@@ -10,10 +13,19 @@
     </a>
     <div class="flex flex-col justify-start items-start gap-1">
         <p class="uppercase font-semibold text-sm">{{ $post->title }}</p>
-        <p class="lowsercase font-thin text-xs flex-center gap-2">
-            {{ $post->author->name }}
-            <x-fas-dot-circle class="w-2 h-2 text-primary" />
-            {{ \Carbon\Carbon::parse($post->published_at)->diffForHumans() }}
-        </p>
+        @if (isset($isBackend))
+            <p class="lowsercase font-thin text-sm flex-center gap-2">
+                Created: {{ Carbon::parse($post->created_at)->toDateTimeString() }}
+            </p>
+            <p class="lowsercase font-thin text-sm flex-center gap-2">
+                Author: {{ $post->author->name }}
+            </p>
+        @else
+            <p class="lowsercase font-thin text-sm flex-center gap-2">
+                {{ $post->author->name }}
+                <x-fas-dot-circle class="w-2 h-2 text-primary" />
+                {{ Carbon::parse($post->published_at)->diffForHumans() }}
+            </p>
+        @endif
     </div>
 </article>
